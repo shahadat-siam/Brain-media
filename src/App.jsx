ff309f1409361266c8694447b268cdce7cf7a7e4
shahadat-siam <lonelinessismycompanion09@gmail.com>
2024-07-13 16:14:38 +0600

    initial setup and navbar

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+ import Menu from '../src/Component/Navbar'
+ 
+ const App = () => {
+  return (
+    <div> 
+      <Menu/> 
+    </div>
+  );
+ };
+ 
+ export default App;
---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,36 @@
+import { IoMdClose } from "react-icons/io";
+import { RiMenu2Line } from "react-icons/ri";
+import "./Navbar.css";  
+const Navbar = () => {
+  const hundleMenuBar = () => {
+    const nav = document.getElementById("navbar");
+    nav.classList.add("active");
+  };
+  const removeMenu = () => {
+    const nav = document.getElementById("navbar");
+    nav.classList.remove("active");
+  };
+
+  return (
+    <>
+     <section id="header">
+        <a href="#" className="text-4xl font-semibold">Brain Media</a>
+        <div>
+            <ul id="navbar">
+                <li><a href="#" className="active">Home</a></li>
+                <li><a href="# ">About Us</a></li>
+                <li><a href="# ">Mission</a></li>
+                <li><a href="# ">Vision</a></li>
+                <li><a href="#">Contact</a></li> 
+                <a href="#" id="close"><i onClick={removeMenu} >< IoMdClose/></i></a>
+            </ul>
+        </div>
+        <div id="mobile"> 
+            <i id="bar" onClick={hundleMenuBar} ><RiMenu2Line/></i> 
+        </div>
+     </section>
+    </>
+  );
+};
+
+export default Navbar;
